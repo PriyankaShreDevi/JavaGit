aa620c120aa1f3d92c926b866faa6b321710d74f
PriyankaShreDevi <shretkd813@gmail.com>
2022-12-11 14:56:32 +0530

    New code added

---
+++ src/org/sample/JavaSample.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class JavaSample {
+
+	public static void main(String[] args) {
+		
+		int i = 3;
+		String s = "Git Test";
+		
+		System.out.println(s);
+		System.out.println(i);
+		
+		
+	}
+	
+}
